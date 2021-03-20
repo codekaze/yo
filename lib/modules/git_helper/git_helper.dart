72c85676e3c466789103ec92fd7655c9e71e461a
codekaze <codekaze@gmail.com>
2021-03-20 13:19:04 +0700

    "."

--- lib/modules/git_helper/git_helper.dart
+++ lib/modules/git_helper/git_helper.dart
@@ -3,7 +3,7 @@ import 'package:yo/shared/helper/exec/exec.dart';
 
 class GitHelper {
   static simplePush() {
-    var dateString = DateFormat("d, M").format(DateTime.now());
+    var dateString = DateFormat('EEE, MMM d, ' 'yy').format(DateTime.now());
     var res = exec('git add . && git commit -m "." && git push');
     print(res);
   }
