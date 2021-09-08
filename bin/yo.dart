f2cfcb23dd0fec05fc7d66deb9a4025653b57e38
codekaze <codekaze.id@gmail.com>
2021-09-08 19:33:42 +0700

    Set new version

--- bin/yo.dart
+++ bin/yo.dart
@@ -157,6 +157,25 @@ void main(List<String> args) async {
     print("This command will Check Google Drive");
     print("--------------");
     // Deploy.run(fullArgumentString);
+
+    Directory dir = Directory(
+        "G:\\.shortcut-targets-by-id\\17SywrFvvouStFB5OBRKD8vt8zbS72Y4H\\UI8");
+    dir.list(recursive: true).forEach((f) async {
+      if (f is File) {
+        if (f.path.endsWith(".ini")) return;
+        var shortPath = f.path.split("\\UI8\\")[1];
+        var myPath = "G:\\Shared drives\\MY SHARED DRIVE HHH\\" + shortPath;
+
+        if (!File(myPath).existsSync()) {
+          print("This File not Exists: $myPath");
+          File(f.path).copySync(myPath);
+          return;
+          // await Future.delayed(Duration(seconds: 1000));
+        }
+        // G:\.shortcut-targets-by-id\17SywrFvvouStFB5OBRKD8vt8zbS72Y4H\UI8\UI8
+        // G:\Shared drives\MY SHARED DRIVE HHH\UI8 Files(2018)
+      }
+    });
   }
   //
   else {
