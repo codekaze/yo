5b6fab40e6682732f7fbb761622683db77f9c161
codekaze <codekaze.id@gmail.com>
2021-09-30 22:09:19 +0700

    .

--- lib/modules/devx_clean/devx_clean.dart
+++ lib/modules/devx_clean/devx_clean.dart
@@ -1,16 +1,48 @@
+import 'dart:io';
+
 import 'package:yo/shared/helper/dir/dir.dart';
 import 'package:yo/shared/helper/exec/exec.dart';
 
 class WifeClean {
+  static String getFilePathFromAnalyzerLineString(String line) {
+    var arr = line.split(" ");
+    var path;
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i];
+      if (str.contains("lib\\")) {
+        path = str;
+        break;
+      }
+    }
+
+    path = path.split(":")[0];
+    return path;
+  }
+
   static void run() {
     var test = exec("flutter analyze").toString().trim();
     var arr = test.split("\n");
 
     for (var i = 0; i < arr.length; i++) {
-      var line = arr[i];
-      if (line.indexOf("Unused import:") > -1 ||
-          line.indexOf("is unnecessary because all of the used elements are") >
-              -1) {
+      var line = arr[i].trim();
+
+      //TODO: Enable this
+      if (line.indexOf("is unnecessary because all of the used elements are") >
+          -1) {
+        var str = line;
+        str = str.replaceAll("info - The import of ", "");
+        str = str.split(" is ")[0];
+
+        var path = getFilePathFromAnalyzerLineString(line);
+        var file = dir(path);
+        var content = file.readAsStringSync();
+        content = content.replaceAll("import $str;", "");
+        file.writeAsStringSync(content);
+
+        print("Remove unnecessary import @ ${file.path}");
+      }
+
+      if (line.indexOf("Unused import:") > -1) {
         var str = line;
         str = str.replaceAll("info - Unused import: ", "");
         str = str.replaceAll(" - unused_import", "");
@@ -22,7 +54,6 @@ class WifeClean {
         var file = dir(path);
         var content = file.readAsStringSync();
         content = content.replaceAll("import '${packageString}';", "");
-
         file.writeAsStringSync(content);
 
         print("Remove unused import @ ${file.path}");
