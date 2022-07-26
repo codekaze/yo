3fe124c1dfa87768ad4ca7ba49288db2c057912c
codekaze <codekaze.id@gmail.com>
2022-07-26 16:55:10 +0700

    .

--- lib/data/config.dart
+++ lib/data/config.dart
@@ -4,10 +4,10 @@ String generatedProjectPath = "C:\\Users\\denyo\\Documents\\FLUTTER_PROJECT";
 String docsUrl = "http://3.142.140.182/docs/";
 
 List registeredProjects = [
-  // "${generatedProjectPath}\\generated\\barber_app",
+  "${generatedProjectPath}\\generated\\barber_app",
   "${generatedProjectPath}\\generated\\car_rental_app",
-  // "${generatedProjectPath}\\generated\\doctor_appointment_app",
-  // "${generatedProjectPath}\\codekaze_pos"
+  "${generatedProjectPath}\\generated\\doctor_appointment_app",
+  "${generatedProjectPath}\\codekaze_pos"
 ];
 
 String BOOKING_API_DIR =
